e0eec4eeed84b825c28a3df29a0493bd7c512cc7
Lujain Ibrahim <li431@nyu.edu>
2019-11-16 16:01:26 +0400

    added nyt api code

---
+++ js/nyt.js
@@ -0,0 +1,31 @@
+function loadData() {
+
+    var $body = $('body');
+    var $header = $('#nytimes-header');
+    var $element = $('#nytimes-articles');
+    var $intro = $('#intro');
+    $element.text("");
+    var topic = $('#topic').val();
+    var address = topic;
+
+    var nytimesUrl = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + topic + '&fq=news_desk:("Environment" "Weather" "Energy" "Cars" "Health" "Politics" "Science")&sort=newest&api-key=QAdGEFleVAbnIm0FVAXfWo5ixuFJooLM'
+    $.getJSON(nytimesUrl, function(data){
+        $header.text('New York Times articles on ' + topic);
+        articles = data.response.docs;
+        for (var i = 0; i < articles.length; i++) {
+            var article = articles[i];
+            $element.append('<li class="article">'+
+                '<a href="'+article.web_url+'">'+article.headline.main+'</a>'+
+                '<p>' + article.snippet + '</p>'+
+                '</li>');
+        };
+    }).error(function(e){
+        $header.text('Sorry we found nothing' );
+
+    })
+
+
+    return false;
+};
+
+$('#form-container').submit(loadData);
