5c74d61adf25f48b1e6c682f5d11f418bc24539b
Brandon Chin Loy <bcl320@nyu.edu>
2019-12-01 17:44:50 +0400

    small changes to print articles in ftimes test

--- js/ft.js
+++ js/ft.js
@@ -1,16 +1,24 @@
+var $element = $('#results');
+$element.text("");
+
 $('#go').click(function() {
     var search = $('#entry').val();
     $.ajax({
         type: "POST",
-        url: "ftapi.php",
+        url: "./php/ftapi.php",
         data: {
           search_term: search
         },
         success: function(data) {
             console.log("Success!");
             // console.log(data);
-            var obj = JSON.parse(data);
-            console.log(obj.results[0].results[0].title.title);
+            var object = JSON.parse(data);
+            for (var i = 0; i < 10; i++) { // To Be Changed
+              $element.append('<li class="article">'+
+              '<a href="'+object.results[0].results[i].location.uri+'">'+object.results[0].results[i].title.title+'</a>'+
+              '<p>' + object.results[0].results[i].summary.excerpt + '</p>'+
+              '</li>');
+            }
         },
         error: function() {
             console.log("Failed!");
