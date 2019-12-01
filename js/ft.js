a818c72f2a03e7045737f62fe1c9d96ad80e0a01
Brandon Chin Loy <bcl320@nyu.edu>
2019-12-01 17:24:34 +0400

    added POST request in the API call

--- js/ft.js
+++ js/ft.js
@@ -1,1 +1,19 @@
-// in progress
+$('#go').click(function() {
+    var search = $('#entry').val();
+    $.ajax({
+        type: "POST",
+        url: "ftapi.php",
+        data: {
+          search_term: search
+        },
+        success: function(data) {
+            console.log("Success!");
+            // console.log(data);
+            var obj = JSON.parse(data);
+            console.log(obj.results[0].results[0].title.title);
+        },
+        error: function() {
+            console.log("Failed!");
+        }
+      });
+});
