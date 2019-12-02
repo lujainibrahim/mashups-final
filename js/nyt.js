989128b59c27f5b0e747c93ae178d1af762f7f73
Lujain Ibrahim <li431@nyu.edu>
2019-12-02 22:09:40 +0400

    fixed nyt api problem

--- js/nyt.js
+++ js/nyt.js
@@ -4,11 +4,13 @@ $('#go').click(function(){
         topic = topic.replace(/ /g,  "%20");
         topic = topic.replace(":", "%3A");
 
-        var path ="http://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + topic + "&sort=newest&api-key=KPy5JG9ziKw1HNABXJqS5cOzXsWMhhfF";
+        var path ="http://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + topic + "&sort=newest&api-key=QAdGEFleVAbnIm0FVAXfWo5ixuFJooLM";
+        console.log(path);
         var selector_string = "#result";
         var $element = $(selector_string)
 
         $.getJSON(path, function(data){
+           $element.html("");
             articles = data.response.docs;
            for (var i = 0; i < 3; i++) {
                 var article = articles[i];
