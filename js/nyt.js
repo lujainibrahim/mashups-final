874c5ce147578d8446738945f0240d1393c65fed
Lujain Ibrahim <36662843+lujainibrahim@users.noreply.github.com>
2019-12-09 12:42:34 +0400

    Update nyt.js

--- js/nyt.js
+++ js/nyt.js
@@ -1,23 +1,23 @@
 $('#go').click(function(){
         var topic =  $('#entry').val();
         console.log(topic);
-        topic = topic.replace(/ /g,  "%20");
-        topic = topic.replace(":", "%3A");
+        topic = topic.replace(/ /g,  "%20"); // replace any spaces
+        topic = topic.replace(":", "%3A"); // replace any colons
 
-        var path ="http://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + topic + "&sort=newest&api-key=QAdGEFleVAbnIm0FVAXfWo5ixuFJooLM";
+        var path ="http://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + topic + "&sort=newest&api-key=QAdGEFleVAbnIm0FVAXfWo5ixuFJooLM"; // API call
         console.log(path);
         var selector_string = "#result";
         var $element = $(selector_string)
 
         $.getJSON(path, function(data){
-           $element.html("");
+           $element.html(""); // clear the div
             articles = data.response.docs;
            for (var i = 0; i < 3; i++) {
                 var article = articles[i];
                 $element.append('<li class="article">'+
                     '<a href="'+article.web_url+'" target="_blank">'+article.headline.main+'</a>'+
                     '<p>' + article.snippet + '</p>'+
-                    '</li>');
+                    '</li>'); // append the div in the main html page to display results
                   }
                 }
               )
